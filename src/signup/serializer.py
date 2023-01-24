27444d989ed0bbb8455a7d6e3c5c24199061523d
gogo christian <gogochristian009@gmail.com>
2023-01-24 10:24:53 +0100

    Commit 24/01/2023

--- src/signup/serializer.py
+++
@@ -1,9 +0,0 @@
-
-from rest_framework import serializers
-
-from .modelsOld import UserFedrelay
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserFedrelay
-        fields = ["email","phone","password","is_active"]
